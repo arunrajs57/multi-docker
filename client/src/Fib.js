ff9eebcb7c4ba2552893b8202a0ad20be2c41320
sonam bhandari <sonambhandari@sonams-MacBook-Pro.local>
2020-07-05 19:37:57 -0400

    dummy change

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -14,7 +14,6 @@ class Fib extends Component {
     }
 
     async fetchValues() {
-        console.log("fetchValues");
       const values = await axios.get('/api/values/current');
       this.setState({values : values.data});
     }
